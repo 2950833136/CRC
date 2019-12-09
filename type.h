54091758e423b35f211be6af3ec7d8a22781d9d8
2950833136 <45315371+2950833136@users.noreply.github.com>
2019-12-10 00:51:57 +0800

    Add files via upload

---
+++ type.h
@@ -0,0 +1,13 @@
+#ifndef __TYPE_H__
+#define __TYPE_H__
+#include <stdio.h>
+
+typedef unsigned char        u8;
+typedef unsigned short       u16;
+typedef unsigned int         u32;
+typedef unsigned long	     u64;
+
+#define FALSE                0
+#define TRUE                 1
+
+#endif
