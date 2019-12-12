e41021fcce26c9dfadc74f8e8496752438dbbdf7
2950833136 <45315371+2950833136@users.noreply.github.com>
2019-12-12 13:22:59 +0800

    Update type.h

--- type.h
+++ type.h
@@ -2,10 +2,14 @@
 #define __TYPE_H__
 #include <stdio.h>
 
+/*****************************************************************************
+*function: 自定义字符长度 
+*type	 : 8 bit/ 16 bit/ 32 bit/ 64 bit
+******************************************************************************/
 typedef unsigned char        u8;
 typedef unsigned short       u16;
 typedef unsigned int         u32;
-typedef unsigned long	     u64;
+typedef unsigned long	       u64;
 
 #define FALSE                0
 #define TRUE                 1
